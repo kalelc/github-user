c4f66c061277ad690712bb02e51b781b00c7a311
Andres Colonia <andreskal@gmail.com>
2019-01-10 23:41:33 -0300

    update user controller

--- user_controller.go
+++ user_controller.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"html/template"
 	"net/http"
 )
@@ -20,23 +18,12 @@ func Index(w http.ResponseWriter, r *http.Request) {
 
 func Result(w http.ResponseWriter, r *http.Request) {
 	name := r.FormValue("name")
-	resp, err := http.Get(usersURL + "?q=" + name)
+
+	var result GithubSearchEngine
+	err := result.Search(name)
 
 	if err != nil {
 		http.Redirect(w, r, "/", http.StatusSeeOther)
-		return
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		resp.Body.Close()
-		return
-	}
-
-	var result UsersSearchResult
-
-	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
-		fmt.Println(err)
-		resp.Body.Close()
 		return
 	}
 
